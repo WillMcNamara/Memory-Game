ccef3d66b2a719d35eee588c71ee8f5891707b79
Will M <wmcna97@gmail.com>
2019-05-28 22:59:50 -0400

    Updates

---
+++ precache-manifest.1d13cd4dd9630a2543d1e35289523598.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981a27eb2221a9c91447930ce888ed76",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "ee47623d3181f7b495cf",
+    "url": "/Memory-Game/static/css/main.bffae65b.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Memory-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ee47623d3181f7b495cf",
+    "url": "/Memory-Game/static/js/main.16aa18c9.chunk.js"
+  },
+  {
+    "revision": "8d83c22dcc2611df7753",
+    "url": "/Memory-Game/static/js/runtime~main.81d8dd5c.js"
+  }
+]);
